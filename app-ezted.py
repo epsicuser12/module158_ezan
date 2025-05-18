62c35e8822399e863b02dfd3662273b3b0179044
epsicuser12 <pm61aks@eduvaud.ch>
2025-05-18 15:25:02 +0200

    user test merge

--- app-ezted.py
+++ app-ezted.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, Devops. ttest"
+    return "Hello, Devops. test"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
